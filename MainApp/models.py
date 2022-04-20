60a90db08d854328c90e7db5d452288409acea3e
“Aurelie” <aurelie_boutin1@baylor.edu>
2022-04-20 14:17:06 -0500

    started django

--- MainApp/models.py
+++ MainApp/models.py
@@ -8,5 +8,14 @@ class Topic(models.Model):  #inheriting all the properties of the models class
 
     def __str__(self): #returns back whatever you want it to return, instead of gibberish
         return self.text
-        
+
+# add a new model
+
+class Entry(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE) # allows us to link these two # entry is a foreign key to topic
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.text[:50]}...'
 
